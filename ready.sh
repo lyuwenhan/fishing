046928b021de85d09b3d6d255ddc1f76aced2165
lyuwenhan <lyuwenhan@gmail.com>
2025-05-22 21:32:49 -0400

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 stty -echo raw
-clear
 stty echo cooked
 echo "Compiling..."
 stty -echo raw
 g++ .code/main.cpp -Wall -o main.run -O2 > /dev/null 2>&1
 if [ $? -ne 0 ];then
-	clear
 	stty echo cooked
 	echo "编译错误，正在显示编译结果"
 	g++ .code/main.cpp -o main.run
@@ -14,5 +12,4 @@ if [ $? -ne 0 ];then
 	read
 	exit 1
 fi
-clear
 stty echo cooked
